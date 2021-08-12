13a26b42470cccda38a4885e0c20fdfc146e6568
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2021-08-12 17:46:23 +0200

    create api for sauce

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const sauceRoutes = require('./routes/sauce');
+
+mongoose.connect('mongodb+srv://newuser4:mongodb4user@cluster0.f2e9r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
-app.use((req, res) => {
-    res.json({ message: 'Votre requête a bien été reçue !' })
-});
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+})
+
+app.use(bodyParser.json());
+
+app.use('/api/sauces', sauceRoutes);
 
 module.exports = app;
---
+++ backend/routes/sauce.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const sauceCtrl = require('../controllers/sauce');
+const sauce = require('../models/sauce');
+
+router.get('/', sauceCtrl.getAllSauces);
+router.post('/', sauceCtrl.createSauce);
+router.get('/:id', sauceCtrl.getOneSauce);
+router.put('/:id', sauceCtrl.modifySauce);
+router.delete('/:id', sauceCtrl.deleteSauce);
+
+module.exports = router;
