60e01de25fddd13ef1f21e58005d0de45c0ab4c2
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2021-08-19 16:28:27 +0200

    fix signup and login error

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,18 +1,14 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const CryptoJS = require('crypto-js');
-const validator = require('validator');
 
-const User = require('../models/user'); 
+const User = require('../models/user');
 
 exports.signup = (req, res, next) => {
-    const encryptedEmail = CryptoJS.AES.encrypt(req.body.email, process.env.EMAIL_ENCRYPTION_KEY).toString();
-    console.log(encryptedEmail);
-    if(validator.isEmail(req.body.email, {blacklisted_chars: '$="'})){
-        bcrypt.hash(req.body.password, 10)
+    bcrypt.hash(req.body.password, 10)
         .then(hash => {
             const user = new User({
-                email: encryptedEmail,
+                email: CryptoJS.MD5(req.body.email).toString(),
                 password: hash
             });
             user.save()
@@ -20,24 +16,19 @@ exports.signup = (req, res, next) => {
                 .catch(error => res.status(500).json({ error }));
         })
         .catch(error => res.status(500).json({ error }));
-    }else{
-        res.status(400).json({error: "Le format de l'adresse e-mail n'est pas valide"});
-    }
 };
 
 exports.login = (req, res, next) => {
-    const decrypted = CryptoJS.AES.decrypt(process.env.EMAIL_CRYPTED, process.env.EMAIL_ENCRYPTION_KEY);
-    const decryptedEmail = decrypt.toString(CryptoJS.enc.Utf8);
-    console.log(decryptedEmail);
-    User.findOne({ email: decryptedEmail })
+    let encryptedMail = CryptoJS.MD5(req.body.email).toString();
+    User.findOne({ email: encryptedMail })
         .then(user => {
-            if(!user) {
+            if (!user) {
                 return res.status(401).json({ error: 'Utilisateur non trouvé !' })
             }
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
-                    if(!valid) {
-                        return res.status(401).json({ error: 'Mot de passe incorrect !' })
+                    if (!valid) {
+                        return res.status(401).json({ error: 'Mot de passe ou email incorrect !' })
                     }
                     res.status(200).json({
                         userId: user._id,
@@ -50,5 +41,5 @@ exports.login = (req, res, next) => {
                 })
                 .catch(error => res.status(500).json({ error }));
         })
-        .catch(error => res.status(500).json({ error }));
 };
+
