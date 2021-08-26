84f2967199e8cca300f223d025b5f859c553304e
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2021-08-26 18:57:34 +0200

    comment code

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,12 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 require('dotenv/config');
 
+// routes for the sauces
 const sauceRoutes = require('./routes/sauce');
+// routes for user
 const userRoutes = require('./routes/user');
 
+// connect to mongoDB
 mongoose.connect(process.env.DB_CONNECTION,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -18,8 +21,10 @@ mongoose.set('useCreateIndex', true);
 
 const app = express();
 
+// secure express app with http headers
 app.use(helmet());
 
+// security so that we can access the API correctly and send requests with the CRUD method
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -27,9 +32,11 @@ app.use((req, res, next) => {
   next();
 })
 
+// needed to handle POST request from front-end, in order to extract JSON object from the request
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// replace prohibited characters in keys with "_"
 app.use(
   mongoSanitize({
     replaceWith: '_',
